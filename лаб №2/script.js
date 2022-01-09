c76bfd809b8a3c4e8973e44f30d427ee79c87af9
Влад <zaykawashe@gmail.com>
2022-01-09 20:56:56 +0200

    _

---
+++ лаб №2/script.js
@@ -0,0 +1,53 @@
+function swap () {
+  var x = document.getElementById("x").value;
+  var y = document.getElementById("y").value;
+  var temp = x;
+  document.getElementById("x").value = y;
+  document.getElementById("y").value = temp;
+}
+
+function square() {
+  var r = 15;
+  const PI = 3.14;
+  document.getElementById("square-result").value = PI * r * r;
+}
+
+function maxCount() {
+  var nums = document.getElementById("square-result").value;
+  var numsSepareted = nums.split(" ");
+  
+  var max = numsSepareted[0];
+  var maxCount = 1;
+  numsSepareted.forEach(x => {
+    if (parseInt(x) > parseInt(max)) {
+      max = x;
+      maxCount = 1;
+    } else {
+      maxCount++;
+    }
+  });
+  
+  window.localStorage.setItem("maxCount", maxCount);
+  alert(maxCount);
+}
+
+function onload() {
+  var res;
+  if(window.localStorage.getItem("maxCount")) {
+    res = prompt("Do you want to delete cookie?",                                     window.localStorage.getItem("maxCount"));
+  }
+  
+  if(res) {
+    window.localStorage.removeItem("maxCount");
+  }
+  
+  if(window.localStorage.getItem("blur")) {
+   document.getElementById("box2").style.background = window.localStorage.getItem("blur"); 
+  }
+}
+
+function blur() {
+  var blur = document.getElementById("blur").value;
+  window.localStorage.setItem("blur", blur)
+  document.getElementById("box2").style.background = blur;
+}
